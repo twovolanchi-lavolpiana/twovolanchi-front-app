6272340e42f5d6dcfa6229ccedacd93bf84983ca
gosekose <ksyn1611@naver.com>
2024-07-11 14:04:38 +0900

    칩 아이콘 적용

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,24 +1,52 @@
 import '../css/Card.css';
 import { OutlinedButton } from "./OutlinedButton";
 import { Stack } from "@mui/material";
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
+import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined';
+import NavigateNextOutlinedIcon from '@mui/icons-material/NavigateNextOutlined';
+import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
+
+type LeftAlignedChipProps = {
+    icon: React.ReactElement;
+    label: string;
+    [key: string]: any; // 다른 모든 추가 props를 받기 위해 사용
+};
+
+
+const LeftAlignedChip: React.FC<LeftAlignedChipProps> = ({ icon, label, ...props }) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                width: '100%',
+            }}
+        >
+            <Chip
+                icon={icon}
+                label={label}
+                sx={{
+                    justifyContent: 'flex-start',
+                    width: '100%',
+                }}
+                {...props}
+            />
+        </Box>
+    );
+};
 
 export const Card = () => {
     return (
         <div className='cards'>
             <div className="cards__container">
                 <Stack direction="column" spacing={2}>
-                    <OutlinedButton
-                        text="패스 하기"
-                        onClick={(e) => console.log(e)}
-                    />
-                    <OutlinedButton
-                        text="시뮬레이션"
-                        onClick={(e) => console.log(e)}
-                    />
-                    <OutlinedButton
-                        text="다음 시퀀스"
-                        onClick={(e) => console.log(e)}
-                    />
+                    <Chip variant="outlined" color="primary" icon={<SkipNextOutlinedIcon />} label="Simulation" />
+                    <Chip variant="outlined" color="primary" icon={<NavigateNextOutlinedIcon />} label="Next Sequence" />
+                    {/* <LeftAlignedChip variant="outlined" color="primary" icon={<SportsSoccerOutlinedIcon />} label="Pass" />
+                    <LeftAlignedChip variant="outlined" color="primary" icon={<SkipNextOutlinedIcon />} label="Simulation" />
+                    <LeftAlignedChip variant="outlined" color="primary" icon={<NavigateNextOutlinedIcon />} label="Next Sequence" /> */}
                 </Stack>
             </div>
         </div>
--- src/component/PlayerCard.tsx
+++ src/component/PlayerCard.tsx
@@ -1,15 +1,37 @@
 import '../css/Card.css';
 import { Stack } from "@mui/material";
 import Chip from '@mui/material/Chip';
-import FaceIcon from '@mui/icons-material/Face';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 export const PlayerCard = () => {
+
+    const handleRedTeamClick = () => {
+        console.log("Red 플레이어 추가")
+    };
+
+    const handleBlueTeamClick = () => {
+        console.log("Blue 플레이어 추가")
+    };
+
+
     return (
         <div className='cards'>
             <div className="cards__container">
                 <Stack direction="column" spacing={2}>
-                    <Chip variant="outlined" color="error" icon={<FaceIcon />} label="Red Team Player"/>
-                    <Chip variant="outlined" color="primary" icon={<FaceIcon />} label="Blue Team Player"/>
+                    <Chip
+                        variant="outlined"
+                        color="error"
+                        icon={<AddCircleOutlineOutlinedIcon />}
+                        label="Red Team Player"
+                        onClick={handleRedTeamClick}
+                    />
+                    <Chip
+                        variant="outlined"
+                        color="primary"
+                        icon={<AddCircleOutlineOutlinedIcon />}
+                        label="Blue Team Player"
+                        onClick={handleBlueTeamClick}
+                    />
                 </Stack>
             </div>
         </div>
